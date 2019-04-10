41be132de047a937557482a564fa58f2b5f21ff8
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-04-10 01:42:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
+/* fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+*/
+
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks';
+  fetch(repo, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`}
+    }
+).then(res => res.json()).then(json => showResults(json));
 }
 
-function showResults(json) {
-  //use this function to display the results from forking via the API
+
+function showResults(el) {
+  document.getElementById('results').innerHTML = `<a href= ${el.html_url} data-issuesurl="`+ el.issues_url +`">` + el.html_url + `</a>`;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const el = document.querySelector('#results a')
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+debugger
+ fetch(el.dataset.issuesurl.replace('{/number}',''),
+      {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: {
+                  Authorization: `token ${getToken()}`
+                }
+      }
+   ).then(res => console.log(res));
+
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const el = document.querySelector('#results a');
+  const issues = fetch(el.dataset.issuesurl.replace('{/number}',''),
+      {
+        method: 'GET',
+        headers: {
+            Authorization: `token ${getToken()}`
+                }
+      }).then(res => res.json()).then(json => document.getElementById('issues').innerHTML = json);
+      debugger
+
+  const issuesList = `<ul> ${json.map(issue => '<li>' + i.title + i.body + '</li>').join('')}</ul>`;
+  /*    document.getElementById('issues').innerHTML = issuesList */
 }
